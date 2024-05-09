8505e0dac4530913f8ed745daa2d438bb276b0bb
aurora muggianu <auroraemu@gmail.com>
2024-05-09 14:30:20 +0200

    add worker form done

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -8,3 +8,13 @@ class ShiftsController < ApplicationController
   end
   
 end
+
+private
+
+def set_shift
+  @shift = shift.find(params[:id])
+end
+
+def shift_params
+  params.require(:shift).permit(:user_id, :start_date)
+end
--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -6,4 +6,17 @@ class WorkersController < ApplicationController
   def show
     @worker = Worker.find(params[:id])
   end
+
+  def new
+    @worker = Worker.new
+  end
+
+  def create
+    @worker = Worker.new(worker_params)
+    if @worker.save
+      redirect_to worker_path(@worker)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
