1374a490b7d5cd02aef77641559b0afebd500237
aurora muggianu <auroraemu@gmail.com>
2024-05-11 10:34:27 +0200

    seeds updated -foreign key

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
 require 'json'
 require 'date'
 
+# Clear existing data
 Worker.destroy_all
 Shift.destroy_all
-
 data = File.read(Rails.root.join('db', 'data.json'))
-
 parsed_data = JSON.parse(data)
 
-parsed_data['workers'].each do |worker|
-  Worker.create!(worker)
+parsed_data['workers'].each do |worker_data|
+  Worker.create!(worker_data)
 end
 
-parsed_data['shifts'].each do |shift|
-  shift.delete('planning_id')
-  worker = Worker.find_by(id: shift['user_id']) # after error while deploying on heroku
+parsed_data['shifts'].each do |shift_data|
+  shift_data.delete('planning_id')
+
+  worker = Worker.find_by(id: shift_data['user_id'])
 
   if worker.present?
-    Shift.create!(shift.merge(worker: worker))
+    Shift.create!(shift_data.merge(worker: worker))
   else
-    puts "Worker with ID #{shift['user_id']} not found."
+    puts "Worker with ID #{shift_data['user_id']} not found."
   end
 end
